9cef17c29cfacf3edd45dfd259a10d0b4a1ae7c0
Af1991 <152735889+Afer1991@users.noreply.github.com>
2024-10-09 13:01:02 -0400

    Add fibonacci func using recursion

--- fibonacci.js
+++ fibonacci.js
@@ -1,6 +1,6 @@
-function fibs (num) {
+function fibs(num) {
   let array = [];
-  
+
   for (let i = 0; i < num; i++) {
     if (i === 0 || i === 1) {
       array.push(i);
@@ -11,3 +11,18 @@ function fibs (num) {
 
   return array;
 };
+
+function fibsRec(num) {
+  if(num <= 0) {
+    return [];
+  } else if (num === 1) {
+    return [0];
+  } else if (num === 2) {
+    return [0, 1];
+  } else {
+    const fibEl = [fibsRec(num - 1)[num - 2] + fibsRec(num - 1)[num - 3]];
+    return fibsRec(num - 1).concat(fibEl);
+  }
+};
+
+
